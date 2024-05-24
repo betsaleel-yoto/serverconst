27c52d0fc4a0b69614d3464efff152bf274555a5
betsaleel yoto <betsaleelyoto@gmail.com>
2024-05-24 12:22:02 +0200

    mon changement vercel

--- index.js
+++ index.js
@@ -21,6 +21,12 @@ app.use(cors({
   }
 }));
 
+
+
+
+
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'uploads')));
